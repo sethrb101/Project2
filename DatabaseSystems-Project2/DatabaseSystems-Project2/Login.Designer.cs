244406b87af6b82f83a153e4e972228bdb779aa8
Seth Blackburn <sethrb101@gwu.edu>
2022-04-13 00:40:26 -0400

    Added UserLogin form; working

---
+++ DatabaseSystems-Project2/DatabaseSystems-Project2/Login.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DatabaseSystems_Project2
+{
+    partial class Form_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_login = new System.Windows.Forms.Button();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label_username = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(208, 201);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.TabIndex = 0;
+            this.button_login.Text = "Login";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(208, 71);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(142, 20);
+            this.textBox_username.TabIndex = 1;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(208, 139);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(142, 20);
+            this.textBox_password.TabIndex = 2;
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Location = new System.Drawing.Point(114, 74);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(75, 13);
+            this.label_username.TabIndex = 3;
+            this.label_username.Text = "Username (ID)";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(114, 142);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(53, 13);
+            this.label_password.TabIndex = 4;
+            this.label_password.Text = "Password";
+            // 
+            // Form_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 293);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_username);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.button_login);
+            this.Name = "Form_login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label_username;
+        private System.Windows.Forms.Label label_password;
+    }
+}
+
---
+++ DatabaseSystems-Project2/DatabaseSystems-Project2/Login.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//====================================================================
+// add following namespace that are used to connect SQL Database
+using System.Data;
+using System.Data.SqlClient;
+//====================================================================
+
+namespace DatabaseSystems_Project2
+{
+    public partial class Form_login : Form
+    {
+        public Form_login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            //==========================After clicking the "Login" button, your code will do following===================================
+
+
+            // check if the username or password are empty
+            if (textBox_username.Text == "" || textBox_password.Text == "")
+            {
+                //throw out a message if they are empty
+                MessageBox.Show("Please provide Username and Password!");
+                return;
+            }
+            // try: catch the error in your code
+            try
+            {
+
+                // connection string, it is the path/value used to find the database. It can be found in database properties.
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\sethrb101\OneDrive - The George Washington University\Project2\DatabaseSystems-Project2\DatabaseSystems-Project2\Library.mdf;Integrated Security=True";
+
+                //create sql connection called "con", used to connect to Students database
+                SqlConnection con = new SqlConnection(connectionString);
+                // the sql command you want to execute in DBMS
+                SqlCommand cmd = new SqlCommand("SELECT * FROM UserLogin WHERE (stuID = @username OR facID = @username) AND password = @password", con);
+
+                //Assign values to variables. Give TextBox: username -> @username; TextBox: password-> @password
+                cmd.Parameters.AddWithValue("@username", textBox_username.Text);
+                cmd.Parameters.AddWithValue("@password", textBox_password.Text);
+
+                //open the connection to DB
+                con.Open();
+
+                //select records from a database and populate a DataSet with the selected rows.
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+
+                //add rows in data adapter
+                adapt.Fill(ds);
+
+                // close the connection after query
+                con.Close();
+
+                //get the collection of tables in the DataSet.
+                int count = ds.Tables[0].Rows.Count;
+
+                //if count is equal to 1, that means the SQL query get the record., then show frmMain form
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    this.Hide();
+                    // Form_main fm = new Form_main();
+                    //fm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed: Could Not Find Your Account!");
+                }
+            }
+            // catch trow out error message if there is an error
+            catch (Exception ex)
+            {
+                //show the error message
+                MessageBox.Show(ex.Message);
+            }
+            //====================================================================
+        }
+    }
+}
